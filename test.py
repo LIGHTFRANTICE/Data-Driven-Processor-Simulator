c2406bfe4de3a4ebeca3fe124b1924d86dea347b
lightfrantice <lightfrantice@gmail.com>
2024-12-14 01:01:14 +0900

    add a test which convert FP32 data to FP16

--- test.py
+++ test.py
@@ -1,19 +1,27 @@
 from lib.dataStruct import DataStreams
-from lib.node import Node, Const
-from lib.operators import add, sub
+from lib.node import Node
+import lib.operators as opr
 from lib.toYaml import convert2Yaml
 from lib.yaml2Machine import getProgram, getTokens
 # import sys
 import yaml
+
+import numpy as np
 
 def main () :
 
     nodeList: list[Node] = []
 
     # Example usage
-    nodeList.append(Node(name="node0", address='0x0000', calType='uni', func='copy'))
-    # nodeList.append(Node(name="node1", address='0x0001', calType='uni', func='copy'))
+    
+    inputValue = 0.496
 
+    fp16 = np.float16(inputValue)
+    fp16_binary = ''.join(f'{c:08b}' for c in fp16.tobytes()[::-1])  # 转成二进制表示
+
+    fp32 = np.float32(inputValue)
+    fp32_binary = ''.join(f'{c:08b}' for c in fp32.tobytes()[::-1])  # 转成二进制表示
+    fp32_hex = ''.join(f'{byte:02x}' for byte in fp32.tobytes()[::-1]) 
 
     # Create data streams
     data_streams = DataStreams()
@@ -21,13 +29,34 @@ def main () :
     data_streams.add_stream('stream1')
 
     # Add data to streams
-    data_streams.add_data('stream0', 'data0', nodeList[0].name, 'left', '0x3F000000')
-    data_streams.add_data('stream1', 'data1', nodeList[1].name, 'left', '0x3F000000')
-    #data_streams.add_data('stream1', 'data2', value="0x3F000000")
+    data_streams.add_data('stream0', 'data0', value= f'0x{fp32_hex}')
+    data_streams.add_data('stream0', 'data1', value='0x80000000')
+    data_streams.add_data('stream0', 'data2', value="0x7F800000")
+    data_streams.add_data('stream0', 'data3', value="0x00780000")
+    data_streams.add_data('stream0', 'data4', value=f"0x{(127<<23):08X}")
+    data_streams.add_data('stream0', 'data5', value=f"0x{(15<<23):08X}")
+    data_streams.add_data('stream0', 'data6', value="0x0F800000")
 
-    nodeList.append(add(nodeList[0], nodeList[1],'0x0002'))
-    #nodeList.append(sub(nodeList[2], data_streams.streams["stream1"]["data2"], '0x0003'))
-    
+    nodeList.append(opr.copy(data_streams.streams["stream0"]["data0"], '0x0000')) #CP0
+    nodeList.append(opr.copy(nodeList[0], '0x0001')) #CP1
+    nodeList.append(opr.andByBit(data_streams.streams["stream0"]["data1"], nodeList[0], '0x0002'))
+    nodeList.append(opr.andByBit(data_streams.streams["stream0"]["data2"], nodeList[1], '0x0003'))
+    nodeList.append(opr.andByBit(nodeList[1], data_streams.streams["stream0"]["data3"], '0x0004'))
+    nodeList.append(opr.sub(data_streams.streams["stream0"]["data4"], nodeList[3], '0x0005'))
+    nodeList.append(opr.sub(data_streams.streams["stream0"]["data5"], nodeList[5], '0x0006'))
+    nodeList.append(opr.andByBit(nodeList[6], data_streams.streams["stream0"]["data6"], '0x0007'))
+    nodeList.append(opr.orByBit(nodeList[7], nodeList[4], '0x0008'))
+    nodeList.append(opr.orByBit(nodeList[2], nodeList[8], '0x0009'))
+    nodeList.append(opr.out(nodeList[9], '0x000a'))
+
+    data_streams.add_data('stream1', 'data0', node_name="0x0000", flag="left", value=f'0x{fp32_hex}')
+    data_streams.add_data('stream1', 'data1', node_name="0x0002", flag="left", value='0x80000000')
+    data_streams.add_data('stream1', 'data2', node_name="0x0003", flag="left", value="0x7F800000")
+    data_streams.add_data('stream1', 'data3', node_name="0x0004", flag="right", value="0x007FE000")
+    data_streams.add_data('stream1', 'data4', node_name="0x0005", flag="left", value=f"0x{(127<<23):08X}")
+    data_streams.add_data('stream1', 'data5', node_name="0x0006", flag="left", value=f"0x{(15<<23):08X}")
+    data_streams.add_data('stream1', 'data6', node_name="0x0007", flag="right", value="0x0F800000")
+
     for node in nodeList :
         print(node)
 
@@ -42,10 +71,9 @@ def main () :
     with open("test.yaml", 'r') as yml:
         ddp = yaml.safe_load(yml)   
 
-    programLines = getProgram(ddp)
-    tokens = getTokens(ddp)
-
     print(f"{'='*10}PROGRAM{'='*10}")
+    programLines = getProgram(ddp)
+    print()
     for line in programLines.values() :
         print(line)
 
@@ -53,8 +81,30 @@ def main () :
     print()
 
     print(f"{'='*10}TOKENS{'='*10}")
+    tokens = getTokens(ddp)
+    print()
     for token in tokens :
         print(token)
+
+    result = input("result: ")
+
+    result = result[0] + result[4:19]
+
+    print(f"result: {result}")
+    print(f"expect: {fp16_binary}")
+
+    int_value = int(result, 2)
+
+    # 将整数转换为 numpy 的 FP16
+    fp16Result = np.frombuffer(int_value.to_bytes(2, 'little'), dtype=np.float16)[0]
+
+    print(f"expect: {inputValue}")
+    print(f"result: {fp16Result}")
+
+    if (abs(fp16Result - inputValue)<0.001):       
+        print("PASS!!")
+    else:
+        print("ERROR!!")
 
 if __name__ == "__main__" :
     main()
